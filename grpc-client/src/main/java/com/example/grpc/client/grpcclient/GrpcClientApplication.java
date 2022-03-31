026ecf74659bf180821b096c8f9792e9d048d956
Cristian Mina <32262469+cristianminaa@users.noreply.github.com>
2022-03-31 19:27:18 +0100

    Attempt 5 to fix imports

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
@@ -1,48 +1,28 @@
 package com.example.grpc.client.grpcclient;
 
+import com.example.grpc.client.grpcclient.storage.StorageProperties;
+import com.example.grpc.client.grpcclient.storage.StorageService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class GrpcClientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcClientApplication.class, args);
-		/*
-		 * ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-		 * .usePlaintext()
-		 * .build();
-		 * 
-		 * HelloServiceGrpc.HelloServiceBlockingStub stub
-		 * = HelloServiceGrpc.newBlockingStub(channel);
-		 * 
-		 * HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-		 * .setFirstName("Baeldung")
-		 * .setLastName("gRPC")
-		 * .build());
-		 * 
-		 * System.out.println(helloResponse);
-		 * 
-		 * channel.shutdown();
-		 */
+	}
 
-		/*
-		 * ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-		 * .usePlaintext()
-		 * .build();
-		 * 
-		 * HelloServiceGrpc.HelloServiceBlockingStub stub
-		 * = HelloServiceGrpc.newBlockingStub(channel);
-		 * 
-		 * HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-		 * .setFirstName("Baeldung")
-		 * .setLastName("gRPC")
-		 * .build());
-		 * 
-		 * channel.shutdown();
-		 */
-		// SpringApplication.run(GrpcClientApplication.class, args);
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
