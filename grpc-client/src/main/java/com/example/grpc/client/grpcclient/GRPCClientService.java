9478b2ca0fa4d5c72fdfdbc684823dd0cc7bc668
Cristian Mina <32262469+cristianminaa@users.noreply.github.com>
2022-04-01 14:01:25 +0100

    Fixing compile errors pt 3

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -212,7 +212,7 @@ public class GRPCClientService {
 	}
 
 	static double getDeadline(Matrix A1, Matrix A2, MatrixServiceBlockingStub stub, int numberOfBlocks, double deadline) {
-		int deadlineMilis = (int) deadline * 1000;
+		int deadlineMilis = (int) (deadline * 1000);
 		double startTime = System.currentTimeMillis();
 		MatrixResponse temp = stub.multiplyMatrix(MatrixRequest.newBuilder().setA(A1).setB(A2).build());
 		double endTime = System.currentTimeMillis();
